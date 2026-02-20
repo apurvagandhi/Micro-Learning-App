14b071c4703225cf5866e03d5b6dc904d2da39a7
Serenity Buckner <scbuckn2@ncsu.edu>
2026-02-19 23:30:01 -0500

    General Setup: Spring Boot, Config Files, Maven, and local PostgreSQL database

---
+++ api/src/main/java/com/microlearning/api/MicroLearningApiApplication.java
@@ -0,0 +1,13 @@
+package com.microlearning.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroLearningApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroLearningApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/microlearning/api/MicroLearningApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microlearning.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroLearningApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
