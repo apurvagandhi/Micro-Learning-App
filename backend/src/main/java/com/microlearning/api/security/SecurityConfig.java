eed4df0f07c1c7bb9b6f9580dfeb28f4df7b9403
Tabiya Khokhar <tabiyarizwanaya22476@gmail.com>
2026-02-25 21:08:10 -0500

    Fix merge conflict markers in SecurityConfig

--- backend/src/main/java/com/microlearning/api/security/SecurityConfig.java
+++ backend/src/main/java/com/microlearning/api/security/SecurityConfig.java
@@ -3,11 +3,8 @@ package com.microlearning.api.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-<<<<<<< HEAD
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-=======
->>>>>>> origin/Develop
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,12 +20,9 @@ public class SecurityConfig {
       )
       .build();
   }
-<<<<<<< HEAD
 
   @Bean
   PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
-=======
->>>>>>> origin/Develop
 }
