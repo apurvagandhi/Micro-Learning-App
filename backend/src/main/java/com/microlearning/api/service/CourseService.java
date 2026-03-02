5aed16e53748e8e8ffeed0f21f1536783cfa6e40
vrrana <viracoder23@gmail.com>
2026-03-01 20:37:50 -0500

    CourseService - Ariel

--- backend/src/main/java/com/microlearning/api/service/CourseService.java
+++ backend/src/main/java/com/microlearning/api/service/CourseService.java
@@ -1,5 +1,43 @@
 package com.microlearning.api.service;
 
-public class LessonService {
-    
+
+import org.springframework.stereotype.Service;
+
+
+
+
+import com.microlearning.api.dto.Course_DTO;
+import com.microlearning.api.model.Course;
+import com.microlearning.api.repository.CourseRepository;
+import com.microlearning.api.repository.UserRepository;
+
+
+@Service
+public class CourseService {
+
+
+    private final UserRepository userRepository;
+    private final CourseRepository courseRepository;
+
+
+    public CourseService(UserRepository userRepository,
+                         CourseRepository CourseRepository) {
+        this.userRepository = userRepository;
+        this.courseRepository = courseRepository;
+    }
+
+
+    public Course_DTO createCourse(CourseRequest req) {
+
+
+        Course course = new Course();
+        course.setCourseId(req.getCourseId());
+
+
+        Course savedCourse = courseRepository.save(course);
+
+
+        return new Course_DTO("Course loaded successfully",
+                                   savedCourse.getCourseId());
+    }
 }
